7ed57ca429806562d7f772a12b6239fa2e64d746
Felippo Stedile <felippostedile@10-41-49-83.0200.epoa.br.iosda.org>
2023-08-28 16:28:08 -0300

    Criado o cara

---
+++ GameCenter/GameCenter/GameCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameCenterApp.swift
+//  GameCenter
+//
+//  Created by Felippo Stedile on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct GameCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
