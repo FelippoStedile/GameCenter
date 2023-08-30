23a32eb28ee5f36754c494e170978e8a49f50740
Pedro Mezacasa Muller <pedro.mezzacasa@gmail.com>
2023-08-30 14:21:43 -0300

    mudanca ui

--- GameCenter/GameCenter/ActualGame/GameView.swift
+++ GameCenter/GameCenter/ActualGame/GameView.swift
@@ -14,25 +14,30 @@ struct GameView: View {
     
     var body: some View {
         ZStack {
-            
-            Rectangle()
-                .ignoresSafeArea()
-                .foregroundColor(Color(
-                    red: (min(actualGame.currentDifference, 0) / -3600),
-                    green: (max(actualGame.currentDifference, 0) / 3600),
-                    blue: max(0, (360 - abs(actualGame.currentDifference))/360)))
-            
-            VStack {
-                if let totalOpponentAngle = actualGame.opponentGameData.deltaAngle {
-                    let score = actualGame.selfScore - totalOpponentAngle//
-                    var turns = Int(score/360)
-                    let current = score.truncatingRemainder(dividingBy: 360)
-                    Text("Placar atual:\nVoltas: \(turns)\n\(current)")//
-                }
-                else {
-                    Text("JOGANDO!")
-                }
+            if let outcome = actualGame.currentGameOutcome {
+                Rectangle()
+                    .ignoresSafeArea()
+                    .foregroundColor(.purple)
+                Text(outcome == .win ? "YOU WIN!" : "YOU LOSE!")
+            } else {
+                Rectangle()
+                    .ignoresSafeArea()
+                    .foregroundColor(Color(
+                        red: (min(actualGame.currentDifference, 0) / -3600),
+                        green: (max(actualGame.currentDifference, 0) / 3600),
+                        blue: max(0, (360 - abs(actualGame.currentDifference))/360)))
             }
+//            VStack {
+//                if let totalOpponentAngle = actualGame.opponentGameData.deltaAngle {
+//                    let score = actualGame.selfScore - totalOpponentAngle//
+//                    var turns = Int(score/360)
+//                    let current = score.truncatingRemainder(dividingBy: 360)
+//                    Text("Placar atual:\nVoltas: \(turns)\n\(current)")//
+//                }
+//                else {
+//                    Text("JOGANDO!")
+//                }
+//            }
         }
     }
 }
